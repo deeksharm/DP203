44502409165c27e81ba8260af6c42b469b6a106c
deeksharm <49231680+deeksharm@users.noreply.github.com>
2023-02-21 11:10:58 +0530

    Create neweventhubfile.py

---
+++ neweventhubfile.py
@@ -0,0 +1,29 @@
+pip install azure-eventhub
+import asyncio
+
+from azure.eventhub import EventData
+from azure.eventhub.aio import EventHubProducerClient
+
+EVENT_HUB_CONNECTION_STR = "EVENT_HUB_CONNECTION_STR"
+EVENT_HUB_NAME = "EVENT_HUB_NAME"
+
+async def run():
+    # Create a producer client to send messages to the event hub.
+    # Specify a connection string to your event hubs namespace and
+    # the event hub name.
+    producer = EventHubProducerClient.from_connection_string(
+        conn_str=EVENT_HUB_CONNECTION_STR, eventhub_name=EVENT_HUB_NAME
+    )
+    async with producer:
+        # Create a batch.
+        event_data_batch = await producer.create_batch()
+
+        # Add events to the batch.
+        event_data_batch.add(EventData("First event "))
+        event_data_batch.add(EventData("Second event"))
+        event_data_batch.add(EventData("Third event"))
+
+        # Send the batch of events to the event hub.
+        await producer.send_batch(event_data_batch)
+
+asyncio.run(run())
